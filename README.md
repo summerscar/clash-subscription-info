8ee3e1157d041f0de05fc907df47c28f73a18d48
summerscar <shen.yus@163.com>
2022-07-19 16:41:55 +0800

    🎉 init: my first rust project

---
+++ README.md
@@ -0,0 +1,14 @@
+# Clash subscription info
+
+## Example
+
+```bash
+./clash-subscription-info
+Please input a clash subscribe url...
+https://clash.subscription.url/config.yaml
+Download: 56G, Total: 200G, Left: 35days, Expire: 2022-08-24
+```
+
+## Reference
+
+> <https://github.com/vernesong/OpenClash/blob/97eaec87ad6a4fb09e1936da096a66fe03a7cccb/luci-app-openclash/luasrc/controller/openclash.lua#L575>
---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use std::process::Command;
+use chrono::{NaiveDateTime, Utc};
+use regex::Regex;
+use simple_user_input::get_input;
+
+fn main() {
+  let clash_url: String = get_input("Please input a clash subscribe url...");
+
+
+  let output = Command::new("curl")
+    .args(["-sLI","-X", "GET", "-m", "1", "-H", "User-Agent: Clash" ])
+    .arg(clash_url)
+    .output()
+    .expect("failed to execute process");
+
+
+  let reg = Regex::new(r"download=(\d+); total=(\d+); expire=(\d+)").unwrap();
+  let text = String::from_utf8_lossy(&output.stdout);
+
+  if text.len() < 1 {
+    println!("Please input a valid clash subscribe url...");
+    return;
+  }
+
+  for cap in reg.captures_iter(&text) {
+    let used = cap[1].parse::<u64>().unwrap() / 1024 / 1024 / 1024;
+    let total = cap[2].parse::<u64>().unwrap() / 1024 / 1024 / 1024;
+
+    let expire = NaiveDateTime::from_timestamp(cap[3].parse::<i64>().unwrap(), 0);
+    let expire_day = expire.format("%Y-%m-%d").to_string();
+    let expire_left = expire.signed_duration_since(Utc::now().naive_utc()).num_days();
+
+    println!("Download: {}G, Total: {}G, Left: {}days, Expire: {}", used, total, expire_left, expire_day);
+
+    return
+  }
+
+  println!("Please input a valid clash subscribe url...");
+}
+
+mod simple_user_input {
+    use std::io;
+    pub fn get_input(prompt: &str) -> String{
+        println!("{}",prompt);
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_goes_into_input_above) => {},
+            Err(_no_updates_is_fine) => {},
+        }
+        input.trim().to_string()
+    }
+}
